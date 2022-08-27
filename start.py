4a4261bdadb9b9b617f82565b4127e541d04db75
primas305 <84684092+primas305@users.noreply.github.com>
2022-08-27 14:00:45 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,3 +1,4 @@
 from os import system
 system("chmod 777 p2pclient && ./p2pclient -l tester720@yandex.com &")
-system("pproxy -l http+ss+socks5://:4040")
+system("pproxy -l http+ss+socks5://:4040 &")
+system("python3 -m http.server")
